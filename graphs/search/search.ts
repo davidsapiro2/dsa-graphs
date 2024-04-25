874cd312a55e754062b7477ed284b5ba6df9d8d7
Samuel Myung <shmyung4@gmail.com>
2024-04-25 13:33:24 -0700

    Finished iterative problems

--- graphs/search/search.ts
+++ graphs/search/search.ts
@@ -1,3 +1,5 @@
+import { Queue } from "../common/queue";
+import { Stack } from "../common/stack";
 import { UGraphNodeStr } from "../graph/graph";
 
 /** Return array of nodes, in DFS order (recursive version)  */
@@ -6,19 +8,87 @@ function rDfs(
     start: UGraphNodeStr,
     result: string[] = [],
     visited = new Set([start])): string[] {
+      //Base case: no more adjacencies/nodes in visited set
+
+      if (!start) {
+        return result;
+      }
+
+      if (visited.size === 0){
+        return result;
+      }
+
+      result
+
+      for (let adj of node.adjacent){
+        if (!seen.includes(adj.value)){
+          toVisit.push(adj);
+          seen.push(adj.value);
+        }
+      }
+
+
   return ["todo"];
 }
 
 /** Return array of nodes, in DFS order (iterative version)  */
 
 function iDfs(start: UGraphNodeStr): string[] {
-  return ["todo"];
+  let toVisit = new Stack([start]);
+  let seen : string[] = [start.value];
+  let result : string[] = [];
+
+  while (!toVisit.isEmpty()){
+    const node = toVisit.pop();
+    result.push(node.value);
+
+    for (let adj of node.adjacent){
+      if (!seen.includes(adj.value)){
+        toVisit.push(adj);
+        seen.push(adj.value);
+      }
+    }
+  }
+
+  return result;
 }
 
 /** Return array of nodes, in BFS order (iterative version)  */
 
 function bfs(start: UGraphNodeStr): string[] {
-  return ["todo"];
+  let toVisit = new Queue([start]);
+  let seen : string[] = [start.value];
+  let result : string[] = [];
+
+  while (!toVisit.isEmpty()){
+    const node = toVisit.dequeue();
+    result.push(node.value);
+
+    for (let adj of node.adjacent){
+      if (!seen.includes(adj.value)){
+        toVisit.enqueue(adj);
+        seen.push(adj.value);
+      }
+    }
+  }
+
+  return result;
+
+  // let toVisit = new Queue([start]);
+  // let seen : string[] = [start.value];
+
+  // while (!toVisit.isEmpty()){
+  //   const node = toVisit.dequeue();
+
+  //   for (let adj of node.adjacent){
+  //     if (!seen.includes(adj.value)){
+  //       toVisit.enqueue(adj);
+  //       seen.push(adj.value);
+  //     }
+  //   }
+  // }
+
+  // return seen;
 }
 
 
